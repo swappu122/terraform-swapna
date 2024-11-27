d02bd52e276524c45d9c203b42a748db8462013d
swapna <swapna.konapuram@gmail.com>
2024-11-27 23:22:31 +0530

    iam

---
+++ day-8-iam/main.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_policy" "example_policy" {
+  name        = "example_policy"
+  description = "Permissions for EC2"
+  policy      = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+        {
+            Action: "s3:*",
+            Effect: "Allow",
+            Resource: "*"
+        }
+      ]
+    })
+}
+
+### Create IAM role
+resource "aws_iam_role" "example_role" {
+  name = "example_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "examplerole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+### Attach IAM policy to IAM role
+resource "aws_iam_policy_attachment" "policy_attach" {
+  name       = "ec2-s3"
+  roles      = [aws_iam_role.example_role.name]
+  policy_arn = aws_iam_policy.example_policy.arn
+}
+
+### Create instance profile using role
+resource "aws_iam_instance_profile" "example_profile" {
+  name = "example_profile"
+  role = aws_iam_role.example_role.name
+}
+
+### Create EC2 instance and attache IAM role
+resource "aws_instance" "example_instance" {
+  instance_type        = "t2.micro"
+  ami                  = "ami-0453ec754f44f9a4a"
+  key_name = "NV"
+  tags={
+    Name = "test"
+  }
+  iam_instance_profile = aws_iam_instance_profile.example_profile.name
+}
