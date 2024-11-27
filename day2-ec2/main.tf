b76d57a10d82aca56539c84cb13faf3c0401ffea
swapna <swapna.konapuram@gmail.com>
2024-11-27 22:47:50 +0530

    dev

---
+++ day2-ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    tags = {
+      Name = var.Name
+    }
+
+  
+}
---
+++ day2-ec2/terraform.tfvars
@@ -0,0 +1,4 @@
+ami = "ami-0453ec754f44f9a4a"
+instance_type = "t2.micro"
+key_name = "NV"
+Name = "dev"
